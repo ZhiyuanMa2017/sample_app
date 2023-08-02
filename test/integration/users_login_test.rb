6e9dd102b2f4d3733327e2dc3ec13bc27c705812
Zhiyuan Ma <631253076@qq.com>
2023-08-02 19:21:54 -0400

    advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  
+
   def setup
     @user = users(:userone)
   end
-  
+
   test "login with invalid info" do
     get login_path
     assert_template "sessions/new"
@@ -16,7 +16,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
-  
+
   test "login with valid info" do
     get login_path
     post login_path, params: { session: { email: @user.email, password: "password" } }
@@ -27,7 +27,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
-    
+
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
@@ -36,5 +36,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
-  
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+  end
+
 end
