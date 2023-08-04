ff47347eb1f0bbb5abe87556f0d6664f9814578a
Zhiyuan Ma <631253076@qq.com>
2023-08-04 01:31:30 -0400

    user edit, update, index, and destroy

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,7 +21,9 @@ module SessionsHelper
       end
     end
   end
-
+  def current_user?(user)
+    user && user == current_user
+  end
   def logged_in?
     !current_user.nil?
   end
@@ -35,5 +37,9 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
